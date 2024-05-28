b0cf85902e588ae4e26edd260951d46f35d08430
NishadGupta <nishadgupta161@gmail.com>
2024-05-28 17:10:39 -0400

    Update api.ts

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,3 +1,3 @@
-export const API_BASEURL = 'http://localhost:8080/';
+export const API_BASEURL = 'https://18.189.149.18/';
 export const LOGIN_URL = `${API_BASEURL}loginPlayer`;
 export const REGISTER_USER_URL = `${API_BASEURL}registerPlayer`;
